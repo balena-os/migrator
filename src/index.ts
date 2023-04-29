88be26548386496fea332a5ff386da5934de6ce5
Ken Bannister <kb2ma@runbox.com>
2023-04-28 20:26:51 -0700

    Prompt for confirmation before starting
    Also add option for non-interactive operation.
    
    Change-type: minor
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import {run, Args, Command, Flags} from '@oclif/core';
+// Must use CommonJS version of inquirer due to limitations of vercel/pkg.
+import * as inquirer from 'inquirer';
 import { migrator } from 'etcher-sdk';
 
 export default class Migrator extends Command {
@@ -12,7 +14,12 @@ export default class Migrator extends Command {
 		sourceImagePath: Args.string({description: 'balenaOS image path', required: true}),
 	};
 
-	static flags = {};
+	static flags = {
+		noninteractive: Flags.boolean({
+			aliases: [ 'non-interactive' ],
+			default: false,
+		}),
+	};
 
 	async run(): Promise<void> {
 		const {args, flags} = await this.parse(Migrator)
@@ -20,8 +27,20 @@ export default class Migrator extends Command {
 		const deviceName = "\\\\.\\PhysicalDrive0";
 		const efiLabel = "M";
 
+		if (!flags.noninteractive) {
+			console.log("Warning! This tool will overwrite the operating system and all data on this computer.");
+			let responses: any = await inquirer.prompt([{
+				name: 'continue',
+				message: "Continue with migration?",
+				type: 'confirm',
+				default: false,
+			}]);
+			if (!responses.continue) {
+				return;
+			}
+		}
 		migrator.migrate(args.sourceImagePath, winPartition, deviceName, efiLabel)
 			.then(console.log)
 			.catch(console.log);
-  }
+	}
 }
